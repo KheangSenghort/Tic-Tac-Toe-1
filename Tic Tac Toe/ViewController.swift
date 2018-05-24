6e44d454d16b3fe2e5deef0d865b2842b1f264a4
SREEHARI K.V <sreeharivijayan619@gmail.com>
2018-05-24 08:20:11 +0530

    fixed

--- Tic Tac Toe/ViewController.swift
+++ Tic Tac Toe/ViewController.swift
@@ -9,7 +9,62 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var activeplayer = 1 //cross
+    var gamestate = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    var winningcombinations = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,4,8],[0,4,8],[2,4,6]]
+    var isgameactive = true
+    
+    @IBOutlet weak var whowon: UILabel!
+    
+    
+    
+    @IBAction func action(_ sender: AnyObject) {
+        
+        if(gamestate[sender.tag-1] == 0 && isgameactive == true)
+        {
+            gamestate[sender.tag-1] = activeplayer
+            if(activeplayer == 1)
+            {
+                sender.setImage(UIImage(named: "Cross.png"), for: UIControlState())
+                activeplayer = 2
+            }
+            else
+            {
+                sender.setImage(UIImage(named: "Nought.png"), for: UIControlState())
+                activeplayer = 1
+            }
+        }
+        for i in winningcombinations
+        {
+            if(isgameactive == true && gamestate[i[0]] != 0 && gamestate[i[0]] == gamestate[i[1]] && gamestate[i[1]] == gamestate[i[2]])
+            {
+                if(activeplayer == 2){
+                    //print("cross win")
+                    isgameactive = false
+                    whowon.text = " CROSS WON"
+                    
+                }
+                if(activeplayer == 1){
+                    //print("Nought win")
+                    isgameactive = false
+                    whowon.text = " NOUGHT WON"
+                }
+            }
+        }
+    }
+    
+    @IBAction func playagain(_ sender: UIView) {
+        isgameactive = true
+        gamestate = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        whowon.text = " "
+        for j in 1...9
+        {
+            let button = view.viewWithTag(j) as! UIButton
+            button.setImage(nil, for:UIControlState())
+        }
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
